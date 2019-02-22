ef52058fef41d9a4ae99073025a61e8bb9cf1b2e
Unknown <doganierogiorgio@yahoo.com>
2019-02-21 21:09:58 -0500

    Initial framework commit

---
+++ PagerScrollView/PagerScrollView/PagerScrollView.h
@@ -0,0 +1,19 @@
+//
+//  PagerScrollView.h
+//  PagerScrollView
+//
+//  Created by Giorgio Doganiero on 2/21/19.
+//  Copyright © 2019 GiorgioDoganiero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PagerScrollView.
+FOUNDATION_EXPORT double PagerScrollViewVersionNumber;
+
+//! Project version string for PagerScrollView.
+FOUNDATION_EXPORT const unsigned char PagerScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PagerScrollView/PublicHeader.h>
+
+
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # PagerScrollView
+
+Easy to use Pager based on UIScrollView  
+
+## Requirements
+- iOS 8+
+- Xcode 8 or above
+- Swift 3
+
+## Installation
+PagerScrollView is available through CocoaPods.
+
+```
+pod 'PagerScrollView'
+```
